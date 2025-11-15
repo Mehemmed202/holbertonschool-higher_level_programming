b8187eb0e803a5c4f9fa9cdc0f81e020cc568ffb
Mehemmed202 <m3mm3dov857@gmail.com>
2025-11-15 10:35:04 +0000

    m

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for i in range(1,100):
+for i in range( 1, 101 ):
     if i % 3 == 0 and i % 5 == 0:
         print( "FizzBuzz", end=" " )
     elif i % 5==0:
@@ -8,3 +8,4 @@ for i in range(1,100):
         print( "Fizz", end=" " )
     else:
         print( i, end=" " )
+print("$")
