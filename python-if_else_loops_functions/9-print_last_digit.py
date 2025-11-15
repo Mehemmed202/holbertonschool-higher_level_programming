c21e354aca00bedfea6fabe8247ed7b769b56f68
Mehemmed202 <m3mm3dov857@gmail.com>
2025-11-15 11:11:40 +0000

    m

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    last_digit = abs(number) % 10
-    print("{}".format(last_digit), end="")
-    return last_digit
+    for char in str:
+        ascii_val = ord(char)
+        if ascii_val >= 97 and ascii_val <= 122:
+            char_to_print = chr(ascii_val - 32)
+        else:
+            char_to_print = char
+        print("{}".format(char_to_print), end="")
+    print("{}".format(""))
