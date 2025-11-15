57ae3241a52775286dcb1c8f519671814039d6c2
Mehemmed202 <m3mm3dov857@gmail.com>
2025-11-15 10:38:16 +0000

    m

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
-"""FizzBuzz funksiyası üçün modul"""
-
 def fizzbuzz():
-    """
-    1-dən 100-ə qədər rəqəmləri FizzBuzz qaydalarına uyğun çap edir.
-    Hər elementdən sonra bir boşluq qoyulur.
-    """
     for number in range(1, 101):
         if number % 15 == 0:
             print("FizzBuzz ", end="")
