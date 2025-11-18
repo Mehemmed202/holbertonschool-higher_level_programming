772941df526e7666298465e13850642c9f326cc9
Mehemmed202 <m3mm3dov857@gmail.com>
2025-11-18 08:03:51 +0000

    m

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-a=1
-b=2
-def add(a, b):
-    return a+b
+if __name__ == "__main__":
+    a=1
+    b=2
+    def add(a, b):
+        return a+b
 print('{} + {} = {}'.format(a, b, add(a, b)) )
