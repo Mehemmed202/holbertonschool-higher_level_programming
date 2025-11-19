2f37abf65984115613fbf1fe43ce0ebe79e3ffad
Mehemmed202 <m3mm3dov857>
2025-11-19 09:51:28 +0000

    m

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
+        if len(row) == 0:
+            print()
+            continue
         for i in range(0, len(row)):
             if row[i] == row[-1]:
                 print("{:d}".format(row[i]), end="\n")
 
             else:
                 print("{:d}".format(row[i]), end=" ")
+
