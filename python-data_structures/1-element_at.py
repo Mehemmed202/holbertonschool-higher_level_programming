8c991139b6f299941a67dd43209a969f4c80478d
Mehemmed202 <m3mm3dov857>
2025-11-19 08:21:39 +0000

    m

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-my_list = [1, 2, 3, 4, 5]
-idx = 3
 def element_at(my_list, idx):
-    for i in range(0,len(my_list)):
-        if idx < 0 or idx > len(my_list)-1:
+    for i in range(0, len(my_list)):
+        if idx < 0 or idx > len(my_list) - 1:
             return None
         return my_list[idx]
