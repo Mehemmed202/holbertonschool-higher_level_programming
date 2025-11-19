8378b49d5484e49485fa7a466bd7e33a353d9b80
Mehemmed202 <m3mm3dov857>
2025-11-19 08:08:05 +0000

    m

---
+++ python-data_structures/1-element_at.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    for i in range(0,len(my_list)):
+        if i == idx:
+            print("Element at index {:d} is {}".format(idx, element_at(my_list, idx)))
