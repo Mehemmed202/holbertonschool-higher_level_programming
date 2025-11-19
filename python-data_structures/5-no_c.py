b451f020429f8f19e1d6b296bcaf9c9446f7d4cf
Mehemmed202 <m3mm3dov857>
2025-11-19 08:59:46 +0000

    m

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -3,5 +3,5 @@ new_string = ""
 def no_c(my_string):
     for i in my_string:
         if i != "c" or i != "C":
-            new_string.append(i)
+            new_string = new_string + i
     return new_string
