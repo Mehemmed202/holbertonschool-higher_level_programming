ce0bc7d137645951696397c192513b0deb03fa41
Mehemmed202 <m3mm3dov857>
2025-11-19 09:24:29 +0000

    m

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
 def print_matrix_integer(matrix=[[]]):
     for i in range(0, len(matrix)):
         for j in range(0, len(matrix)):
-            if j == matrix[i][j]:
-                print("{:d}".format(j),end="\n")
+            if matrix[i][j] == matrix[i][-1]:
+                print("{:d}".format(matrix[i][j]), end="\n")
             else:
-                print("{:d}".format(j), end=" ")
+                print("{:d}".format(matrix[i][j]), end=" ")
