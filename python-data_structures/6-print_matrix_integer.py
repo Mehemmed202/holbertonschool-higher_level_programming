d290ba5fa641b875ac84e5a6f4f278ec9509597d
Mehemmed202 <m3mm3dov857>
2025-11-19 09:46:05 +0000

    m

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
 def print_matrix_integer(matrix=[[]]):
-    for i in range(0, len(matrix)):
-        for j in range(0, len(matrix)):
-            if matrix[i][j] == matrix[i][-1]:
-                print("{:d}".format(matrix[i][j]), end="\n")
+    for row in matrix:
+        for i in range(0, len(row)):
+            if row[i] == row[-1]:
+                print("{:d}".format(row[i]), end="\n")
+
             else:
-                print("{:d}".format(matrix[i][j]), end=" ")
+                print("{:d}".format(row[i]), end=" ")
