398d676506480d87001779f54fe23365c1c8317d
Mehemmed202 <m3mm3dov857>
2025-11-19 08:33:09 +0000

    m

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-new_element = 9
 def replace_in_list(my_list, idx, element):
     for i in range(0, len(my_list)):
         if idx < 0 or idx > len(my_list) - 1:
             return my_list
-    my_list[idx] = new_element
+    my_list[idx] = element
     return my_list
