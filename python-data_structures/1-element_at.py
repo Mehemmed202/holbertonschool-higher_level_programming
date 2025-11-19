e900b091bf3bb6643780d43cd3f8d1a133c239e4
Mehemmed202 <m3mm3dov857>
2025-11-19 08:17:48 +0000

    m

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -3,5 +3,6 @@ my_list = [1, 2, 3, 4, 5]
 idx = 3
 def element_at(my_list, idx):
     for i in range(0,len(my_list)):
-        if i == idx:
-            print("Element at index {:d} is {}".format(idx, element_at(my_list, idx)))
+        if idx < 0 or idx > len(my_list):
+            return None
+        return mylist[idx]
