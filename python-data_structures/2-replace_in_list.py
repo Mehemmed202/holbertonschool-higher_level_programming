b572b92575faa3bcfe3c2e307c1cdf924330b23d
Mehemmed202 <m3mm3dov857>
2025-11-19 08:29:01 +0000

    m

---
+++ python-data_structures/2-replace_in_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    for i in range(0, len(my_list)):
+        if idx < 0 or idx > len(my_list) - 1:
+            return my_list
+    mylist[3] = new_element
+    return mylist
