d8885546e20f4a78bff1a8b72a6594e1343ea701
Mehemmed022 <m3mm3dov857@gmail.com>
2025-11-20 06:51:26 +0000

    m

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -2,4 +2,5 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     a = (tuple_a + (0, 0))[:2]
     b = (tuple_b + (0, 0))[:2]
-    return ([0] + b[0], a[1] + b[1])
+    result = (a[0] + b[0], a[1] + b[1])
+    return result
