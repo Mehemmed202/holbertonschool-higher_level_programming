90ff22909b6922223b36a52de7db2ca55c322472
Mehemmed202 <m3mm3dov857@gmail.com>
2025-11-20 15:00:04 +0000

    m

---
+++ 0-square_matrix_simple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_matrix = matrix[:]
+    for row in new_matrix:
+        for i in range(len(row)):
+            print(row[i] = row[i]**2)
+    for i
