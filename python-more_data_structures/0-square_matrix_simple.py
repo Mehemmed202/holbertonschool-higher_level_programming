145542cf9e52369396d075a73b576270114060cc
Mehemmed202 <m3mm3dov857@gmail.com>
2025-11-20 20:18:58 +0000

    m

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+    my_matrix = []
+    for row in matrix:
+        my_matrix.append(row)
+        mlist = []
+        for i in row:
+            mlist.append(i * i)
+        new_matrix.append(mlist)
+
+    return new_matrix, my_matrix
