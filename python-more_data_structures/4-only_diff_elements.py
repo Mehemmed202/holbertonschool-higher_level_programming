9d5eddb3c7868d5757cd8221ec6e6dd22b30218a
Mehemmed202 <m3mm3dov857@gmail.com>
2025-11-20 21:07:35 +0000

    m

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
     mylist = []
-    set_1.extend(set_2)
+    (set_1).extend(set_2)
     for word in set_1:
         if (set_1).count(word) == 1:
             mylist.append(word)
