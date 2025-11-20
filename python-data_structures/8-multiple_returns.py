3b660e8f4b2ef6f4ac934c95b23b104c64a1fc6c
Mehemmed202 <m3mm3dov857@gmail.com>
2025-11-20 08:48:35 +0000

    m

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -2,7 +2,7 @@
 def multiple_returns(sentence):
     c = 0;
     for i in sentence:
-        c++
+        c = c + 1
     if c > 0:
         first = sentnce[0]
         print("Length: {} - First character: {}".format(c, first))
