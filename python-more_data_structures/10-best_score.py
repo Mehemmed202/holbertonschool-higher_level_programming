db3b88178541ca9176bff9b19c9a37d23f9d94de
Mehemmed202 <m3mm3dov857@gmail.com>
2025-11-21 20:12:20 +0000

    m

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     k = 0
+    c = 0
     for i in a_dictionary.keys():
         if a_dictionary[i] > k:
+            c++
             k = a_dictionary[i]
+        if c == 0:
+            return None
     for j in a_dictionary:
         if k == a_dictionary[j]:
             return j
