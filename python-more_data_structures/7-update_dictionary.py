32abe253b9677d6ed150de7854d88314e2556195
Mehemmed202 <m3mm3dov857@gmail.com>
2025-11-21 19:04:42 +0000

    m

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -2,5 +2,6 @@
 a_dictionary = { 'language': "C", 'number': 89, 'track': "Low level" }
 def update_dictionary(a_dictionary, key, value):
     a_dictionary.update({key : value})
-    for i in a_dictionary.keys():
+    dct = a_dictionary.keys()
+    for i in dct:
         print("{}: {}".format(key, a_dictionary[key]))
