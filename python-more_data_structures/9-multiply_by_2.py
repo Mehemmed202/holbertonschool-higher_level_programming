69d9f9953fe1ce103b58e32f5227b91c875f01e3
Mehemmed202 <m3mm3dov857@gmail.com>
2025-11-21 19:41:13 +0000

    m

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -2,5 +2,5 @@
 def multiply_by_2(a_dictionary):
     n_dictionary = []
     for i in a_dictionary.keys():
-        n_dictionary[i] = 2 * a_dictionary[i]
+        n_dictionary[int(i)] = 2 * a_dictionary[int(i)]
     return n_dictionary
