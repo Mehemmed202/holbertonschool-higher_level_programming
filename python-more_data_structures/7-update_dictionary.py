62d9ab41c358f782647604af5fba7106ef2a4082
Mehemmed202 <m3mm3dov857@gmail.com>
2025-11-21 19:13:38 +0000

    m

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    a_dictionary.update({key:value})
+    a_dictionary.update({key : value})
     dct = sorted(a_dictionary.keys())
     for i in dct:
         return a_dictionary
