3af0c4e4bbbad9988830416da3fe273a30c2639a
Mehemmed202 <m3mm3dov857@gmail.com>
2025-11-22 10:15:20 +0000

    m

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -5,5 +5,5 @@ def safe_print_list(my_list=[], x=0):
         try:
             k.append(str(my_list[i]))
         except:
-            print(''.join(k) ,k.index(k[-1]))
-    print(''.join(k), k.index(k[-1]))
+            print(''.join(k) ,k.index(k[-1]), end = "\n")
+    print(''.join(k), k.index(k[-1]), end = "\n")
