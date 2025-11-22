ed5990056b1283409fcb8929037b9b204e837f85
Mehemmed202 <m3mm3dov857@gmail.com>
2025-11-22 13:41:20 +0000

    m

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -7,6 +7,7 @@ def safe_print_list_integers(my_list=[], x=0):
             c = c + 1
             k.append(mylist[i])
             print("{:d}".format("".join(k)))
+            print(c)
     except (IndexError, TypeError):
 
 
