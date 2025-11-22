0907f00ddb1c3bfb02bf62a89a32e46f9cfae38a
Mehemmed202 <m3mm3dov857@gmail.com>
2025-11-22 10:08:10 +0000

    m

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    k = ""
+    k = []
     for i in range(x):
         try:
             k.append(str(my_list[i]))
         except:
-            print("{}".format(k))
+            return (''.join(k))
+    return (''.join(k))
