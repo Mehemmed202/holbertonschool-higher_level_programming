f0fefcde245ba074898dd38335f24b5a72b1fc5b
Mehemmed202 <m3mm3dov857@gmail.com>
2025-11-22 09:56:43 +0000

    m

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    k = ""
+    for i in range(x):
+        try:
+            k.append(str(my_list[i]))
+        except:
+            print("{}".format(k))
