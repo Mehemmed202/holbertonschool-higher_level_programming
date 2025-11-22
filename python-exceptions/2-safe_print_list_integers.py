0be348cb4aa1fd83ecdec4c40b163a943ff12a4a
Mehemmed202 <m3mm3dov857@gmail.com>
2025-11-22 13:44:48 +0000

    m

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    k = []
-    c = 0
     try:
         for i in range(x):
             c = c + 1
-            k.append(my_list[i])
-            print("{:d}".format("".join(k)))
-            print(c)
+            print("{:d}".format(my_list[i]), end="")
     except (IndexError, TypeError):
         pass
