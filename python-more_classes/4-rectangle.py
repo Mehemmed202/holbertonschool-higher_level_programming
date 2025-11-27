0c842c4e6dc78248f20bdc3592fe0da24f56941a
Mehemmed202 <m3mm3dov857@gmail.com>
2025-11-27 13:42:08 +0000

    m

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -45,7 +45,7 @@ class Rectangle:
         if self.__height == 0 or self.__width == 0:
             print()
         else:
-            for i in (self.__height):
+            for i in range(self.__height):
                 print(self.__width * "#")
                 if i != self.__height - 1:
                     print()
