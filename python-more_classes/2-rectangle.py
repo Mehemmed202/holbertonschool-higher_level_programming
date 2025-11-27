b4dd8b35a42f9d97a4af36bd7fa479d5d691d648
Mehemmed202 <m3mmdov857@gmail.com>
2025-11-27 07:58:12 +0000

    m

---
+++ python-more_classes/2-rectangle.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""a"""
+
+class Rectangle:
+    """s"""
+
+    def __init__(self, width=, height=0):
+        self.width = width
+        self.height = height
+
+    @propety
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+
+    def area(self):
+        return (width * height)
+
+    def perimeter(self):
+        if width == 0 or height == 0:
+            perimeter = 0
+        else:
+            perimeter  = 2 * (width + height)
+        return perimeter
