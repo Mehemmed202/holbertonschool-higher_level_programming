b8ba660d0b9f3504f4b50407636213dc699d0e89
Mehemmed202 <m3mm3dov857@gmail.com>
2025-11-29 20:36:47 +0000

    m

---
+++ python-input_output/11-student.py
@@ -0,0 +1,24 @@
+#!/bin/python3
+"""a"""
+
+
+def __init__(self, first_name, last_name, age):
+    """a"""
+    self.first_name = first_name
+    self.last_name = last_name
+    self.age = age
+
+    def to_json(self, attrs=None):
+        new_dict = {}
+        my_dict = attr.__dict__
+
+        if isinstance(attr, list) and (isinstance(a, str) for a in my_dict):
+            for k in attrs:
+                if k in my_dict:
+                    new_dict[k] = my_dict[k]
+
+        return new_dict
+
+    def reload_from_json(self, json):
+        for key, value in json.items():
+        setattr(self, key, value)
