2b5b33d86869519bbd4530b2f06804472a879373
Mehemmed202 <m3mm3dov857@gmail.com>
2025-11-29 20:43:26 +0000

    m

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -21,4 +21,4 @@ def __init__(self, first_name, last_name, age):
 
     def reload_from_json(self, json):
         for key, value in json.items():
-        setattr(self, key, value)
+            setattr(self, key, value)
