6b0e3d521d26a6eb387682641f2418c071a03cd2
Mehemmed202 <m3mm3dov857@gmail.com>
2025-11-29 23:36:07 +0000

    m

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""a"""
+
+
+import json
+
+
+class CustomObject:
+    """a"""
+
+
+    def __init__(self, name, age, is_student):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def display(self):
+        for k, v in self.__dict__.items():
+            i = "{}: {}".format(k, v)
+            print(i.capitalize())
+
+    serialize(self, filename):
+        with open(filename, "w", encoding="UTF-8") as f:
+            return pickle.dump(self, f)
+    
+    @classmethod
+    deserialize(cls, filename):
+        with open(filename, "r", encoding="UTF-8") as fl:
+            data = pickle.load(fl)
+        return cls(**data)
