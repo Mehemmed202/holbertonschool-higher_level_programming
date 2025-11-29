ab38a914acbf050e34dd45254099ae2f4ac2cfca
Mehemmed202 <m3mm3dov857@gmail.com>
2025-11-29 21:46:20 +0000

    m

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -3,20 +3,20 @@
 
 
 def pascal_triangle(n):
-    """a""" 
+    """a"""
     if n <= 0:
         return []
 
-    triangle = [[1]] 
+    triangle = [[1]]
 
     for i in range(1, n):
         prev_row = triangle[-1]
-        row = [1] 
+        row = [1]
 
         for j in range(1, i):
             row.append(prev_row[j-1] + prev_row[j])
 
-        row.append(1) 
+        row.append(1)
         triangle.append(row)
 
     return triangle
