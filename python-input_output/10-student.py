1c6e9681371bbc5868ddd4cab4310b7caa3e8145
Mehemmed202 <m3mm3dov857@gmail.com>
2025-11-29 20:18:31 +0000

    m

---
+++ python-input_output/10-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""a"""
+
+
+class Student:
+    """a"""
+
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        my_list = self.__dict__
+        if isinstance(attrs, list) and all(isinstance(attrs, str) for a in attrs):
+            return {k: my_dict[k] for k in attrs if k in my_dict}
+
+        return my_dict
