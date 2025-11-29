b996c85f3bb05e2ab114a33781aa0d19c35579b7
Mehemmed202 <m3mm3dov857@gmail.com>
2025-11-29 23:15:59 +0000

    m

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -13,4 +13,4 @@ def serialize_and_save_to_file(data, filename):
 def load_and_deserialize(filename):
     # Your code here to load and deserialize data from the specified file
     with open(filename, "r", encoding="UTF-8") as fl:
-        return fson.loads(fl)
+        return json.loads(fl)
