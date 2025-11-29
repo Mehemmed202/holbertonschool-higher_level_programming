7855d6cf7422ddfe0d2004add9b1ed540516d738
Mehemmed202 <m3mm3dov857@gmail.com>
2025-11-29 17:37:05 +0000

    m

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -8,5 +8,5 @@ import json
 def load_from_json_file(filename):
     """a"""
 
-    with open(filename, "w", encoding="UTF-8") as f:
+    with open(filename, "r", encoding="UTF-8") as f:
         json.load(f)
