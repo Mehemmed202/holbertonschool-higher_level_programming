ef1ddf322f1c327d5f43529b95bcefc09f66377f
Mehemmed202 <m3mm3dov857@gmail.com>
2025-11-30 08:34:30 +0000

    m

---
+++ python-serialization/task_02_csv.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""a"""
+
+
+import csv
+import json
+
+def convert_csv_to_json(csv_filename):
+    try:
+        data = []
+
+        with open(csv_filename, "r", encoding="UTF-8") as csv_file:
+                csv_reader = csv.DictReader(csv_file)
+                for row in csv_reader:
+                    data.append(row)
+
+        with open(data, "w") as f:
+            json.dump(data, f)
+
+        return True
+
+    except (FileNotFoundError, Exception):
+        return False
