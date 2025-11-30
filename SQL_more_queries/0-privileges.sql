5ae62d3e067a64c75d58f136268de43d328c3e63
Mehemmed202 <m3mm3dov857@gmail.com>
2025-11-30 13:08:49 +0000

    m

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- 0-privileges.sql
-SHOW GRANT ALL PRIVILEGES ON localhost to user_0d_1;
-SHOW GRANT ALL PRIVILEGES ON localhost to user_0d_2;
+SHOW GRANTS PRIVILEGES FOR 'user_0d_1'@'localhost';
+SHOW GRANTS PRIVILEGES FOR 'user_0d_2'@'localhost';
