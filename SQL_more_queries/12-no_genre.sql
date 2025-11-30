a5493ed90642001773f899786ae2d6df6422c7af
Mehemmed202 <m3mm3dov857@gmail.com>
2025-11-30 14:55:31 +0000

    m

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,7 +1,4 @@
--- Script lists all shows contained in hbtn_0d_tvshows without a genre linked.
--- Each record displays: tv_shows.title - tv_show_genres.genre_id
--- Results sort in ascending order by tv_shows.title and tv_show_genres.genre_id
--- It uses only one SELECT statement
+-- sdlm
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
