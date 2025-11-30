6544e0429be088e30afd6ce58e74d08e347ba6aa
Mehemmed202 <m3mm3dov857@gmail.com>
2025-11-30 13:40:23 +0000

    m

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,2 +1,2 @@
 -- 4-never_empty.sql
-CREATE TABLE IF NOT EXISTS id_not_null (id INT, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS id_not_null (id INT DEFAULT 1, name VARCHAR(256));
---
+++ SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,2 @@
+-- 5-unique_id.sql
+CREATE TABLE IF NOT EXIST unique_id 
