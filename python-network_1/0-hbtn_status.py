fc8b9f2166cd1ce3ed8625a5854ec64d0ed2f46e
Mehemmed202 <m3mm3dov857@gmail.com>
2025-12-03 15:35:21 +0000

    m

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+#comment
+
+import urllib.request
+
+if __name__ == __main__:
+    url="https://intranet.hbtn.io/status"
+    with urllib.request.urlopen(url) as f:
+        raw_data = f.read()
+        data = raw_data.decode("utf-8")
+        print("Body response:")
+        print(f"\t- type: {type(raw_data)}")
+        print(f"\t- content: {raw_data}")
+        print(f"\t- utf8 content: {data}")
