dc9e04c50ffece08cf56c14bcebe2bea2f97bd7c
Mehemmed202 <m3mm3dov857@gmail.com>
2025-12-03 21:51:29 +0000

    m

---
+++ python-network_1/8-json_api.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""a"""
+
+import sys
+import requests
+
+if __name__ == __main__:
+    url = "http://0.0.0.0:5000/search_user"
+    if sys.argv[1] >= 1:
+        key = sys.argv[1]
+    else:
+        key = ""
+
+    payload = {"q": key}
+    response = requests.post(url, json=payload)
+
+    try:
+        json_response  =response.json()
+
+        if json_response:
+            print("[{}] {}".format(json_response.get('id'),
+                                   json_response.get('name')))
+        else:
+            print("No result")
+        
+    except ValueError:
+        print("Not a valid JSON")
+
+
