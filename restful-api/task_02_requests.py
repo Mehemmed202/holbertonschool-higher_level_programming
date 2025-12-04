2562a15a779c016157d9ad20315246d38a683faa
Mehemmed202 <m3mm3dov857@gmail.com>
2025-12-04 12:49:26 +0000

    m

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -1,7 +1,24 @@
-import requests
+#!/usr/bin/python3
 
-if __name__ == "__main__":
+import requests
+import csv
+
+fetch_and_print_posts():
     url = "https://jsonplaceholder.typicode.com/posts"
     response = requests.get(url)
-    print(response.status_code)
-    print(response.json())
+    if response.status_code == 200:
+        print("Status Code: 200")
+        posts = response.json()
+        cleaned_posts = [
+            {
+                "id": post["id"],
+                "title": post["title"],
+                "body": post["body"]
+            }
+            for post in posts
+        ]
+
+        with open("posts.csv","w", newline="", encoding="utf-8") as csv_file:
+            writer = csv.DictWriter(csv_file, fieldnames=["id", "tittle", "body"])
+            writer.writeheader()
+            write.writerows(cleaned_posts)
