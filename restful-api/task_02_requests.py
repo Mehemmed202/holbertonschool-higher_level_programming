d2cd14c5a44302d30b7bf038b241529e03dea22d
Mehemmed202 <m3mm3dov857@gmail.com>
2025-12-04 12:18:30 +0000

    m

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -1,1 +1,10 @@
-#!
+#!/usr/bin/python3
+"""a"""
+
+import requests
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url)
+    print(url.status_code)
+    print(url.json())
