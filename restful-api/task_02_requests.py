43e1f8188b528d2dc6e58cbe016865ae3689fbbe
Mehemmed202 <m3mm3dov857@gmail.com>
2025-12-04 12:52:10 +0000

    m

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -3,7 +3,24 @@
 import requests
 import csv
 
-fetch_and_print_posts():
+def fetch_and_print_posts():
+    url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url)
+
+    print(f"Status Code: {response.status_code}")
+
+    if response.status_code == 200:
+        posts = response.json()
+        for post in posts:
+            print(post["title"])
+
+
+
+
+
+
+
+def fetch_and_save_posts():
     url = "https://jsonplaceholder.typicode.com/posts"
     response = requests.get(url)
     if response.status_code == 200:
